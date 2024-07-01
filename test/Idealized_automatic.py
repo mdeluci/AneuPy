2560793b3acfebf2e9aa7ea33d14baa7784d1052
Mario de Lucio <mdeluci@purdue.edu>
2024-07-01 16:45:30 -0400

    Update Idealized_automatic.py

--- test/Idealized_automatic.py
+++ test/Idealized_automatic.py
@@ -1,3 +1,15 @@
+# =============================================================================
+#
+# Idealized_automatic.py
+#
+# Python module to generate idealized AAA geometries automatically
+#
+# Jacobo Diaz - jdiaz@udc.es
+# Mario de Lucio - mdeluci@purdue.edu
+# 2024
+#
+# =============================================================================
+
 #!/usr/bin/env python3
 
 import os
