cd8d1f785065ba12391b4d56531c95ebcfcbbe14
Mario de Lucio <mdeluci@purdue.edu>
2024-07-01 16:32:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,31 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 def readme():
     with open('README.md') as f:
         return f.read()
-
-setup(name='aneupy',
-      version='0.1',
-      packages=['aneupy'],
-      description='Python code for simulating Abdominal Aorta Aneurysms in Abaqus/CAE',
-      long_description=readme(),
-      url='https://github.com/UDC-GME/aneupy',
-      author='Jacobo Diaz',
-      author_email='jdiaz@udc.es',
-      classifiers=[
+setup(
+    name='aneupy',
+    version='0.1.0',
+    author='Mario de Lucio',
+    author_email='mdeluci@purdue.edu',
+    description='A Python library for simulating Abdominal Aortic Aneurysms',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/aneupy',
+    packages=find_packages(),
+    classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Education',
-        'Topic :: Scientific/Engineering',
-        'Environment :: Console',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-      ],
-      license='GPL',
-      install_requires=[
-                'sympy',
-                'numpy',
-                ],
-      include_package_data=True,
-      zip_safe=False)
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',  # Add any other dependencies your project needs
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    zip_safe=False
+)
